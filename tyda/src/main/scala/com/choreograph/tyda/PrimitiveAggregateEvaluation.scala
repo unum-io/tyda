01584ed384ad4799f312aa1ad8218e91dcd10499
Emil Ejbyfeldt <emil.ejbyfeldt@choreograph.com>
2026-05-21 16:27:48 +0200

    Do not store a codec inside Aggregator
    Since we are serializing Aggregators as part of the Spark backend it
    makes sense to keep them as small as possible. The only reason we had
    the codec their in the first place as that Spark needed it, but as
    showed here we can store it "next to" instead of in the Aggreator.

--- tyda/src/main/scala/com/choreograph/tyda/PrimitiveAggregateEvaluation.scala
+++ tyda/src/main/scala/com/choreograph/tyda/PrimitiveAggregateEvaluation.scala
@@ -6,26 +6,50 @@ import com.choreograph.tyda.Aggregator.Reduce
 
 private[tyda] object PrimitiveAggregateEvaluation {
   def aggregator[From, To](agg: PrimitiveAggregate[From, To]): Aggregator[From, ?, To] =
+    aggregatorAndIntermediateCodec(agg).aggregator
+
+  final case class AggregatorAndIntermediateCodec[From, I1, To](
+      aggregator: Aggregator[From, I1, To],
+      codec: Codec[I1]
+  )
+  private def make[From, I: Codec, To](
+      agg: Aggregator[From, I, To]
+  ): AggregatorAndIntermediateCodec[From, I, To] = AggregatorAndIntermediateCodec(agg, Codec[I])
+
+  def aggregatorAndIntermediateCodec[From, To](
+      agg: PrimitiveAggregate[From, To]
+  ): AggregatorAndIntermediateCodec[From, ?, To] =
     agg match {
       case PrimitiveAggregate.Collect() =>
         given Codec[To] = agg.codec
-        Compose(Seq(_), Reduce[Seq[From]](_ ++ _))
-      case PrimitiveAggregate.Count() => Compose(_ => 1L, Reduce[Long](_ + _))
-      case PrimitiveAggregate.CountSome() => Compose(t => if t.isDefined then 1L else 0L, Reduce[Long](_ + _))
-      case PrimitiveAggregate.BoolAnd() => Reduce[Boolean](_ && _)
-      case PrimitiveAggregate.BoolOr() => Reduce[Boolean](_ || _)
-      case PrimitiveAggregate.Min(comparable) => Reduce(comparableToOrd(comparable).min)(using agg.codec)
-      case PrimitiveAggregate.Max(comparable) => Reduce(comparableToOrd(comparable).max)(using agg.codec)
+        make(Compose(Seq(_), Reduce[Seq[From]](_ ++ _)))
+      case PrimitiveAggregate.Count() => make(Compose(_ => 1L, Reduce[Long](_ + _)))
+      case PrimitiveAggregate.CountSome() =>
+        make(Compose(t => if t.isDefined then 1L else 0L, Reduce[Long](_ + _)))
+      case PrimitiveAggregate.BoolAnd() => make(Reduce[Boolean](_ && _))
+      case PrimitiveAggregate.BoolOr() => make(Reduce[Boolean](_ || _))
+      case PrimitiveAggregate.Min(comparable) =>
+        given Codec[To] = agg.codec
+        make(Reduce(comparableToOrd(comparable).min))
+      case PrimitiveAggregate.Max(comparable) =>
+        given Codec[To] = agg.codec
+        make(Reduce(comparableToOrd(comparable).max))
       case minBy: PrimitiveAggregate.MinBy[?, ?] =>
-        minByAggregator(comparableToOrd(minBy.comparable), minBy.inputCodec)
+        minByAggregator(comparableToOrd(minBy.comparable))(using minBy.inputCodec)
       case maxBy: PrimitiveAggregate.MaxBy[?, ?] =>
-        minByAggregator(comparableToOrd(maxBy.comparable).reverse, maxBy.inputCodec)
-      case PrimitiveAggregate.Reduce(f) => Reduce(f)(using agg.codec)
-      case PrimitiveAggregate.Sum(magnet) => Compose(magnet.toResult, Reduce(magnet.add)(using magnet.codec))
+        minByAggregator(comparableToOrd(maxBy.comparable).reverse)(using maxBy.inputCodec)
+      case PrimitiveAggregate.Reduce(f) =>
+        given Codec[To] = agg.codec
+        make(Reduce(f))
+      case PrimitiveAggregate.Sum(magnet) =>
+        given Codec[To] = magnet.codec
+        make(Compose(magnet.toResult, Reduce(magnet.add)))
     }
 
-  def minByAggregator[V, O](ord: Ord[O], codec: Codec[(V, O)]): Aggregator[(V, O), ?, V] =
-    AndThen(Reduce[(V, O)]((a, b) => if ord.lt(a._2, b._2) then a else b)(using codec), _._1)
+  def minByAggregator[V, O](
+      ord: Ord[O]
+  )(using codec: Codec[(V, O)]): AggregatorAndIntermediateCodec[(V, O), ?, V] =
+    make(AndThen(Reduce[(V, O)]((a, b) => if ord.lt(a._2, b._2) then a else b), _._1))
 
   def comparableToOrd[T](comparable: Comparable[T]): Ord[T] =
     comparable match {
