03c135f22c0f3036aad85645cd3534f192f2531d
Emil Ejbyfeldt <emil.ejbyfeldt@choreograph.com>
2026-06-04 13:06:49 +0200

    Do not store a codec inside Aggregator (#17)
    Since we are serializing Aggregators as part of the Spark backend it
    makes sense to keep them as small as possible. The only reason we had
    the codec their in the first place as that Spark needed it, but as
    showed here we can store it "next to" instead of in the Aggreator.

---
+++ tyda/src/test/scala/com/choreograph/tyda/AggregatorSpec.scala
@@ -0,0 +1,65 @@
+package com.choreograph.tyda
+
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import java.io.ObjectStreamClass
+
+import scala.util.Using
+
+import org.scalatest.funsuite.AnyFunSuite
+
+object AggregatorSpec {
+  def assertDoesNotCapture(value: Serializable, forbiddenClasses: Class[?]*): Unit = {
+    val bytes = Using.resource(ByteArrayOutputStream()) { baos =>
+      Using.resource(ObjectOutputStream(baos))(_.writeObject(value))
+      baos.toByteArray
+    }
+    Using.resource(ByteArrayInputStream(bytes)) { bais =>
+      val ois = new ObjectInputStream(bais) {
+        override def resolveClass(desc: ObjectStreamClass): Class[?] = {
+          val cls = super.resolveClass(desc)
+          forbiddenClasses.foreach { forbidden =>
+            assert(
+              !forbidden.isAssignableFrom(cls),
+              s"${forbidden.getSimpleName} subtype found in serialized value: ${desc.getName}"
+            )
+          }
+          cls
+        }
+      }
+      ois.readObject(): Unit
+    }
+  }
+}
+
+class AggregatorSpec extends AnyFunSuite {
+  import AggregatorSpec.*
+
+  def testAggregatorDoesNotCaptureCodec[P <: PrimitiveAggregate[?, ?]: TypeName](agg: P): Unit =
+    test(s"Aggregator for ${TypeName.name[P]} does not serialize any Codec") {
+      assertDoesNotCapture(
+        PrimitiveAggregateEvaluation.aggregatorAndIntermediateCodec(agg).aggregator,
+        classOf[Codec[?]]
+      )
+    }
+
+  test("asstDoesNotCapture should throw on forbiddenidden class") {
+    assertThrows[AssertionError] { assertDoesNotCapture(Tuple1(Codec[Long]), classOf[Codec[?]]) }
+  }
+
+  testAggregatorDoesNotCaptureCodec(PrimitiveAggregate.Collect[Long]())
+  testAggregatorDoesNotCaptureCodec(PrimitiveAggregate.Collect[Option[Long]]())
+  testAggregatorDoesNotCaptureCodec(PrimitiveAggregate.Collect[(a: Int, b: String)]())
+  testAggregatorDoesNotCaptureCodec(PrimitiveAggregate.Count[Long]())
+  testAggregatorDoesNotCaptureCodec(PrimitiveAggregate.CountSome[Long]())
+  testAggregatorDoesNotCaptureCodec(PrimitiveAggregate.BoolAnd())
+  testAggregatorDoesNotCaptureCodec(PrimitiveAggregate.BoolOr())
+  testAggregatorDoesNotCaptureCodec(PrimitiveAggregate.Min[Long](Comparable.long))
+  testAggregatorDoesNotCaptureCodec(PrimitiveAggregate.Max[Long](Comparable.long))
+  testAggregatorDoesNotCaptureCodec(PrimitiveAggregate.MinBy[Long, Long](Comparable.long))
+  testAggregatorDoesNotCaptureCodec(PrimitiveAggregate.MaxBy[Long, Long](Comparable.long))
+  testAggregatorDoesNotCaptureCodec(PrimitiveAggregate.Reduce[Long](_ + _))
+  testAggregatorDoesNotCaptureCodec(PrimitiveAggregate.Sum(SumMagnet[Long]))
+}
