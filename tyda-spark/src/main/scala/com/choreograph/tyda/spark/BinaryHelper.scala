7185c75ae89c4e5d370ba2018b50e55e30c9bb47
Jonas Dahlbæk <30782351+dahlbaek@users.noreply.github.com>
2026-06-08 22:55:24 +0200

    Move BinaryHelper

--- tyda-spark/src/main/scala/com/choreograph/tyda/spark/BinaryHelper.scala
+++
@@ -1,8 +0,0 @@
-package com.choreograph.tyda.spark
-
-import com.choreograph.tyda.Binary
-
-private[spark] object BinaryHelper {
-  def fromArray(bytes: Array[Byte]): Binary = Binary.fromArray(bytes)
-  def toArray(b: Binary): Array[Byte] = b.to(Array)
-}
