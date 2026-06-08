967585dc5edfc248d4bd0f627865f83203a2886e
Emil Ejbyfeldt <emil.ejbyfeldt@choreograph.com>
2026-06-08 11:34:16 +0200

    Use opaque types in AllSingletons
    The this mainly some clean up. I don't think we considered this pattern
    when the code was originally written. But I think leads to simpler and
    clearer code.

--- tyda/src/main/scala/com/choreograph/tyda/AllSingletons.scala
+++ tyda/src/main/scala/com/choreograph/tyda/AllSingletons.scala
@@ -4,32 +4,20 @@ import scala.deriving.Mirror
 /** A type class that provides access and serves as evidence that all values of
   * a enum are singletons.
   */
-trait AllSingletons[T] {
-  def values: Seq[T]
-}
+opaque type AllSingletons[T] <: Seq[T] = Seq[T]
 
 object AllSingletons {
   def apply[T: AllSingletons]: AllSingletons[T] = summon
 
   given [T](using m: Mirror.SumOf[T], tuple: AllSingletonsTuple[T, m.MirroredElemTypes]): AllSingletons[T] =
-    new AllSingletons[T] {
-      def values: Seq[T] = tuple.values
-    }
+    tuple
 
-  trait AllSingletonsTuple[T, Elem <: Tuple] {
-    def values: List[T]
-  }
+  opaque type AllSingletonsTuple[T, Elem <: Tuple] = List[T]
 
   object AllSingletonsTuple {
-    given empty[T]: AllSingletonsTuple[T, EmptyTuple] with {
-      def values: List[T] = Nil
-    }
-
-    given [T, H <: T: ValueOf, Tail <: Tuple](using tail: AllSingletonsTuple[T, Tail]): AllSingletonsTuple[
-      T,
-      H *: Tail
-    ] with {
-      def values: List[T] = valueOf[H] :: tail.values
-    }
+    given empty[T]: AllSingletonsTuple[T, EmptyTuple] = Nil
+    given [T, H <: T: ValueOf, Tail <: Tuple](using
+        tail: AllSingletonsTuple[T, Tail]
+    ): AllSingletonsTuple[T, H *: Tail] = valueOf[H] :: tail
   }
 }
