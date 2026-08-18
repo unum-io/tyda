fa54d682f4ed006366ab91ce2b560e15e8039436
Jonas Dahlbæk <30782351+dahlbaek@users.noreply.github.com>
2026-08-18 11:57:44 +0200

    cleanup

--- tyda/src/main/scala/com/choreograph/tyda/ExprApi.scala
+++ tyda/src/main/scala/com/choreograph/tyda/ExprApi.scala
@@ -83,11 +83,9 @@ trait ExprApi[Expr[T]] {
     @targetName("optionMap")
     def map[U, I: AsExpr.Of[U]](f: Expr[T] => I): Expr[Option[U]] = when(!isEmpty, f(get))
 
-    /** Returns this Option if the predicate returns true or the option is
-      * empty, otherwise returns None.
-   /** Returns this Option if it is nonempty and applying the predicate `p` to this Option's 
-     * value returns true. Otherwise, return None.
-    */
+    /** Returns this Option if it is nonempty and applying the predicate `p` to
+      * this Option's value returns true. Otherwise, return None.
+      */
     @targetName("optionFilter")
     def filter(p: Expr[T] => Expr[Boolean]): Expr[Option[T]] = when(!isEmpty && p(get), get)
 
